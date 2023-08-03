55b9f47e882ccf7de271d5467c44167ee913b5a6
rajan <rajanshahi1267@gmail.com>
2023-08-03 05:12:08 -0700

    add anoyher botton

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,20 +1,11 @@
 
 import "./home.scss"
-// import Stories from '../../components/stories/Stories'
 import Posts from '../../components/posts/Posts'
 import Profile from '../profile/Profile'
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Register from './../register/Register';
-
 const Home = () => {
   return (
     <div className='homeBar'>
       <Profile />
-      <BrowserRouter>
-        <Routes>
-          <Route path="/register/rajan" element={<Register/>} />
-        </Routes>
-      </BrowserRouter>
       {/* <Stories/> */}
       <Posts />
     </div>
